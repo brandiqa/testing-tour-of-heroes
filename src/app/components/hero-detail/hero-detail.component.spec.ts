d751e2aaae5c8878d30499d9d242e0d7bcbba4b8
Michael Wanyoike <micwanyoike@gmail.com>
2018-02-21 15:31:17 +0300

    Fixed failing tests

--- src/app/components/hero-detail/hero-detail.component.spec.ts
+++ src/app/components/hero-detail/hero-detail.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { HeroDetailComponent } from './hero-detail.component';
 
@@ -8,7 +9,8 @@ describe('HeroDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroDetailComponent ]
+      declarations: [ HeroDetailComponent ],
+      imports: [ FormsModule]
     })
     .compileComponents();
   }));
--- src/app/components/heroes/heroes.component.spec.ts
+++ src/app/components/heroes/heroes.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { HeroesComponent } from './heroes.component';
 
@@ -10,7 +10,7 @@ describe('HeroesComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ HeroesComponent ],
-      imports: [ FormsModule]
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
