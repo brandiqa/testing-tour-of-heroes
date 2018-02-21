599f54a39bc6abd58a66b5275b94a514bf12f677
Michael Wanyoike <micwanyoike@gmail.com>
2018-02-21 12:03:41 +0300

    Fixed all broken tests

--- src/app/components/heroes/heroes.component.spec.ts
+++ src/app/components/heroes/heroes.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { HeroesComponent } from './heroes.component';
 
@@ -8,7 +9,8 @@ describe('HeroesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroesComponent ]
+      declarations: [ HeroesComponent ],
+      imports: [ FormsModule]
     })
     .compileComponents();
   }));
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-heroes',
