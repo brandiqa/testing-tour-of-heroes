da7790a3abd641dd45c6180281b38093f19a8c2d
Michael Wanyoike <micwanyoike@gmail.com>
2018-02-21 11:14:52 +0300

    Initialized Heroes Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
