51516b8c3e965ce1e5615698078ff831fc4ed4d5
Michael Wanyoike <micwanyoike@gmail.com>
2018-02-21 19:39:28 +0300

    Added route to dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HeroesComponent }      from '@components/heroes/heroes.component';
+import { DashboardComponent }   from '@components/dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'heroes', component: HeroesComponent }
 ];
 
