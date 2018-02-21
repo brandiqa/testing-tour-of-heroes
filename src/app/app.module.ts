5fb9a2e78b6d3f918f01f23fd0a9ea0f3fc2202e
Michael Wanyoike <micwanyoike@gmail.com>
2018-02-21 18:33:42 +0300

    Initialized Message Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 import { HeroService } from '@services/hero.service';
+import { MessagesComponent } from './components/messages/messages.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
