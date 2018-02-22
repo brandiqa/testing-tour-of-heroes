72682a7b1f5f21a322b7e05059301f387d982fb7
Michael Wanyoike <micwanyoike@gmail.com>
2018-02-22 17:43:25 +0300

    Fixed Dashboard spec

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { DashboardComponent } from './dashboard.component';
+import { HeroService } from '@services/hero.service';
+import { MockHeroService } from '@services/hero.service.mock';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -8,7 +12,15 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      imports: [ RouterTestingModule ],
+      declarations: [ DashboardComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ],
+      providers: [
+        {
+          provide: HeroService,
+          useClass: MockHeroService
+        }
+      ],
     })
     .compileComponents();
   }));
