b9a5f31403a3b0158488648da4d73e7269d1269c
Michael Wanyoike <micwanyoike@gmail.com>
2018-02-23 17:31:09 +0300

    Implemented test for updateHero

--- src/app/services/hero.service.spec.ts
+++ src/app/services/hero.service.spec.ts
@@ -11,6 +11,16 @@ describe('HeroService', () => {
   let service;
   let httpTestingController: HttpTestingController;
 
+  let mockHeroes: Hero[];
+
+  beforeEach(() => {
+    mockHeroes = [
+      { id: 1, name: 'Hulk' },
+      { id: 2, name: 'Thor'},
+      { id: 3, name: 'Iron Man'}
+    ] as Hero[];
+  });
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -25,32 +35,42 @@ describe('HeroService', () => {
     service = s;
   }));
 
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
+
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
   describe('getHeroes', () => {
-    let mockHeroes: Hero[];
-
-    beforeEach(() => {
-      mockHeroes = [
-        { id: 1, name: 'Hulk' },
-        { id: 2, name: 'Thor'},
-        { id: 2, name: 'Iron Man'}
-      ];
-    });
-
     it('should return mock heroes', () => {
       service.getHeroes().subscribe(
         heroes => expect(heroes).toEqual(mockHeroes),
         fail
       );
-      // Respond to GET request
+      // Receive GET request
       const req = httpTestingController.expectOne(service.heroesUrl);
       expect(req.request.method).toEqual('GET');
 
       // Respond with the mock heroes
       req.flush(mockHeroes);
+    });
+  });
+
+  describe('updateHero', () => {
+    it('should update hero', () => {
+      const updateHero: Hero = { id: 1, name: 'Superman' };
+      service.updateHero(updateHero).subscribe(
+        response => expect(response).toEqual(updateHero),
+        fail
+      );
+      // Receive PUT request
+      const req = httpTestingController.expectOne(service.heroesUrl);
+      expect(req.request.method).toEqual('PUT');
+      // Respond with the updated hero
+      req.flush({id: 1, name: 'Superman' });
     });
   });
 
