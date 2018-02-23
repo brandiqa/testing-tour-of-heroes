a1a97fe6c4a360d4c9be0f162b6c7313c0bceacc
Michael Wanyoike <micwanyoike@gmail.com>
2018-02-23 16:28:49 +0300

    Implemented getHeroes test

--- src/app/services/hero.service.spec.ts
+++ src/app/services/hero.service.spec.ts
@@ -1,22 +1,24 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientModule, HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { HeroService } from './hero.service';
 import { MessageService } from './message.service';
+import { Hero } from '@models/hero.model';
 
-fdescribe('HeroService', () => {
+describe('HeroService', () => {
 
   let service;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        HttpClientModule,
         HttpClientTestingModule
       ],
       providers: [HeroService, MessageService]
     });
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   beforeEach(inject([HeroService], s => {
@@ -26,5 +28,31 @@ fdescribe('HeroService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('getHeroes', () => {
+    let mockHeroes: Hero[];
+
+    beforeEach(() => {
+      mockHeroes = [
+        { id: 1, name: 'Hulk' },
+        { id: 2, name: 'Thor'},
+        { id: 2, name: 'Iron Man'}
+      ];
+    });
+
+    it('should return mock heroes', () => {
+      service.getHeroes().subscribe(
+        heroes => expect(heroes).toEqual(mockHeroes),
+        fail
+      );
+      // Respond to GET request
+      const req = httpTestingController.expectOne(service.heroesUrl);
+      expect(req.request.method).toEqual('GET');
+
+      // Respond with the mock heroes
+      req.flush(mockHeroes);
+    });
+  });
+
 
 });
