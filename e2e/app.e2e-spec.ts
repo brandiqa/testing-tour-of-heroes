0c702dc62cf64a5d188aa7a045805ddf3b79310b
Michael Wanyoike <micwanyoike@gmail.com>
2018-02-25 17:37:18 +0300

    Fixed e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser  } from 'protractor';
 
 describe('angular-tour-of-heroes App', () => {
   let page: AppPage;
@@ -7,8 +8,14 @@ describe('angular-tour-of-heroes App', () => {
     page = new AppPage();
   });
 
+  it('should redirect to dashboard', async () => {
+    page.navigateTo();
+    const url = await browser.getCurrentUrl();
+    expect(url).toContain('/dashboard');
+  });
+
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Tour of Heroes');
   });
 });
